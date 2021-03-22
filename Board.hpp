8df797cf5634bbeedc60ba610141b0dc10a8bbef
Matan Ben Nagar <matannagar@gmail.com>
2021-03-22 17:45:01 +0200

    fixed make-tidy

--- Board.hpp
+++ Board.hpp
@@ -8,32 +8,28 @@
 using namespace std;
 using ariel::Direction;
 
-namespace ariel{
+namespace ariel
+{
 
   class Board
   {
-     vector<vector<char>> board={
+    vector<vector<char>> board = {
         {'_', '_', '_'},
         {'_', '_', '_'},
-        {'_', '_', '_'}
-    };;
-      // std::array<std::array<string,rows>,columns> board;
+        {'_', '_', '_'}};
+    ;
+    // std::array<std::array<string,rows>,columns> board;
 
-      public:
-        Board(){
-          // cout<<"i have been built"<<endl;
-          // vector<vector<char>> board={{'_','_','_'},
-          //                               {'_','_','_'},
-          //                               {'_','_','_'}};
-        };
-        
-        // ~Board(){cout<<"Board destroyed"<<endl;}
-         void post(unsigned int a, unsigned int b,Direction d, const string &message);
-         std::string read(unsigned int a, unsigned int b,Direction d, unsigned int message);
-         void show();
-         void clear();
+  public:
+    Board(){};
+
+    ~Board() { cout << "Board destroyed" << endl; }
+    void post(unsigned int row, unsigned int column, Direction d, const string &message);
+    std::string read(unsigned int row, unsigned int column, Direction d, unsigned int message);
+    void show();
+    void clear();
   };
- 
+
 }
 
 #endif
--- Demo.cpp
+++ Demo.cpp
@@ -15,24 +15,19 @@ using namespace std;
 
 int main() {
 	ariel::Board board;
-	// board.post(/*row=+1*/10, /*column=*/10, Direction::Horizontal, "abcd");
-	// cout <<board.read(/*row=*/10, /*column=*/10, Direction::Vertical, /*length=*/3) <<  endl;  
+	board.post(/*row=+1*/10, /*column=*/10, Direction::Horizontal, "abcd");
+	cout <<board.read(/*row=*/10, /*column=*/10, Direction::Vertical, /*length=*/3) <<  endl;  
 	// 	// prints "_b_" (starts at row 99 which is empty; then at row 100 there is "b"; then row 101 is empty again).
-	// board.post(/*row=*/10, /*column=*/10, Direction::Vertical, "xyz");
-	// cout << board.read(/*row=*/10, /*column=*/10, Direction::Horizontal, /*length=*/6) << endl;  
+	board.post(/*row=*/10, /*column=*/10, Direction::Vertical, "xyz");
+	cout << board.read(/*row=*/10, /*column=*/10, Direction::Horizontal, /*length=*/6) << endl;  
 	// 	// prints "abyd__" (First letters are ab; then y from the "xyz"; then d; then two empty cells).
 
-	// board.show(); // shows the board in a reasonable way. For example:
+	board.show(); // shows the board in a reasonable way. For example:
 		//    98:  _________
 		//    99:  ____x____
 		//    100: __abyd___
 		//    101: ____z____
 		//    102: _________
-board.post(0,0,Direction::Vertical,"abc");
-board.post(0,1,Direction::Vertical,"def");
-board.post(0,2,Direction::Vertical,"ghi");
-board.show();
-//read the entire board letter by letter
-cout<<board.read(0,1,Direction::Vertical,1)<<endl;
+
 }
 
