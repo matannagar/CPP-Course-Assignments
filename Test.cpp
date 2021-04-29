95c1b87da27fb8de54fb96fb2fc19eabb274d74e
Matan Ben Nagar <73398186+matannagar@users.noreply.github.com>
2021-04-29 15:12:43 +0300

    Add files via upload

--- Test.cpp
+++ Test.cpp
@@ -15,5 +15,61 @@ using namespace std; //used to include functions such as memcpy..
 
 TEST_CASE("read_units")
 {
-cout<<"nothing yet"<<endl;
+	Board board;  // Initialize an empty board (with 0 disease cubes in any city).
+	board[City::Kinshasa] = 3;      // put 3 yellow disease cubes in Kinshasa.
+	board[City::Kinshasa] = 2;      // change number of disease cubes in Kinshasa to 2.
+	board[City::MexicoCity] = 3;    // put 3 yellow disease cubes in MexicoCity
+	board[City::HoChiMinhCity] = 1; // put 1 red disease cube in HoChiMinhCity
+	board[City::Chicago] = 1; 
+CHECK_NOTHROW(board[City::Kinshasa] = 2);
+CHECK_NOTHROW(board[City::Kinshasa] = 2);
+CHECK_NOTHROW(board[City::Kinshasa] = 2);
+CHECK_NOTHROW(board[City::Kinshasa] = 2);
+CHECK_NOTHROW(board[City::Kinshasa] = 2);
+CHECK_NOTHROW(board[City::Kinshasa] = 2);
+CHECK_NOTHROW(board[City::Kinshasa] = 2);
+CHECK_NOTHROW(board[City::Kinshasa] = 2);
+CHECK_NOTHROW(board[City::Kinshasa] = 2);
+CHECK_NOTHROW(board[City::Kinshasa] = 2);
+CHECK_NOTHROW(board[City::Kinshasa] = 2);
+CHECK_NOTHROW(board[City::Kinshasa] = 2);
+CHECK_NOTHROW(board[City::Kinshasa] = 2);
+CHECK_NOTHROW(board[City::Kinshasa] = 2);
+CHECK_NOTHROW(board[City::Kinshasa] = 2);
+CHECK_NOTHROW(board[City::Kinshasa] = 2);
+CHECK_NOTHROW(board[City::Kinshasa] = 2);
+CHECK_NOTHROW(board[City::Kinshasa] = 2);
+CHECK_NOTHROW(board[City::Kinshasa] = 2);
+CHECK_NOTHROW(board[City::Kinshasa] = 2);
+CHECK_NOTHROW(board[City::Kinshasa] = 2);
+CHECK_NOTHROW(board[City::Kinshasa] = 2);
+CHECK_NOTHROW(board[City::Kinshasa] = 2);
+CHECK_NOTHROW(board[City::Kinshasa] = 2);
+CHECK_NOTHROW(board[City::Kinshasa] = 2);
+CHECK_NOTHROW(board[City::Kinshasa] = 2);
+CHECK_NOTHROW(board[City::Kinshasa] = 2);
+CHECK_NOTHROW(board[City::Kinshasa] = 2);
+CHECK_NOTHROW(board[City::Kinshasa] = 2);
+CHECK_NOTHROW(board[City::Kinshasa] = 2);
+CHECK_NOTHROW(board[City::Kinshasa] = 2);
+CHECK_NOTHROW(board[City::Kinshasa] = 2);
+CHECK_NOTHROW(board[City::Kinshasa] = 2);
+CHECK_NOTHROW(board[City::Kinshasa] = 2);
+CHECK_NOTHROW(board[City::Kinshasa] = 2);
+CHECK_NOTHROW(board[City::Kinshasa] = 2);
+CHECK_NOTHROW(board[City::Kinshasa] = 2);
+CHECK_NOTHROW(board[City::Kinshasa] = 2);
+CHECK_NOTHROW(board[City::Kinshasa] = 2);
+CHECK_NOTHROW(board[City::Kinshasa] = 2);
+CHECK_NOTHROW(board[City::Kinshasa] = 2);
+CHECK_NOTHROW(board[City::Kinshasa] = 2);
+CHECK_NOTHROW(board[City::Kinshasa] = 2);
+CHECK_NOTHROW(board[City::Kinshasa] = 2);
+CHECK_NOTHROW(board[City::Kinshasa] = 2);
+CHECK_NOTHROW(board[City::Kinshasa] = 2);
+CHECK_NOTHROW(board[City::Kinshasa] = 2);
+CHECK_NOTHROW(board[City::Kinshasa] = 2);
+CHECK_NOTHROW(board[City::Kinshasa] = 2);
+CHECK_NOTHROW(board[City::Kinshasa] = 2);
+
 }
